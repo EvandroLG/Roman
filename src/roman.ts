3d92b348438469fa6404efcff3ea06ae4b8f21f3
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-11-15 00:17:53 +0100

    create function to convert number to roman

--- src/roman.ts
+++ src/roman.ts
@@ -1,16 +1,35 @@
 const SYMBOLS: { [key: string]: number } = {
-  I: 1,
-  V: 5,
-  X: 10,
-  L: 50,
-  C: 100,
-  D: 500,
   M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
 };
 
 const Roman = {
   isRoman() {},
-  toRoman() {},
+
+  toRoman(value: number) {
+    let concat = '';
+    let copyValue = value;
+
+    for (const key of Object.keys(SYMBOLS)) {
+      while (copyValue >= SYMBOLS[key]) {
+        concat = concat + key;
+        copyValue = copyValue - SYMBOLS[key];
+      }
+    }
+
+    return concat;
+  },
 
   toNumber(value: string) {
     let sum = 0;
--- test/roman.test.ts
+++ test/roman.test.ts
@@ -1,6 +1,6 @@
 import Roman from '../src';
 
-test('romanToNumber', () => {
+test('toNumber', () => {
   expect(Roman.toNumber('III')).toBe(3);
   expect(Roman.toNumber('IV')).toBe(4);
   expect(Roman.toNumber('IX')).toBe(9);
@@ -10,4 +10,16 @@ test('romanToNumber', () => {
   expect(Roman.toNumber('CM')).toBe(900);
   expect(Roman.toNumber('LVIII')).toBe(58);
   expect(Roman.toNumber('MCMXCIV')).toBe(1994);
+});
+
+test('toRoman', () => {
+  expect(Roman.toRoman(3)).toBe('III');
+  expect(Roman.toRoman(4)).toBe('IV');
+  expect(Roman.toRoman(9)).toBe('IX');
+  expect(Roman.toRoman(40)).toBe('XL');
+  expect(Roman.toRoman(90)).toBe('XC');
+  expect(Roman.toRoman(400)).toBe('CD');
+  expect(Roman.toRoman(900)).toBe('CM');
+  expect(Roman.toRoman(58)).toBe('LVIII');
+  expect(Roman.toRoman(1994)).toBe('MCMXCIV');
 });
